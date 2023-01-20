5374e44c7cba5dd53fb4799893f18e6de1c38493
ioioio0719 <kio0719@docomo.ne.jp>
2023-01-20 14:53:11 +0900

    ユーザーがアイコン画像を設定できるようにするため、画像アップロード機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,34 @@ class UsersController < ApplicationController
   def profile
     @user = current_user
   end
+  def profile_edit
+    @user = current_user
+  end
+  def profile_update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_profile_path
+    else
+      render "profile_update"
+    end
+  end
   def account
     @user = current_user
+  end
+  def account_edit
+    @user = current_user
+  end
+  def account_update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_account_path
+    else
+      render "account_edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:icon)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   get 'users/index'
   get 'users/profile'
+  get 'users/profile/edit', to: 'users#profile_edit'
+  patch 'users/profile/update', to: 'users#profile_update'
   get 'users/account'
+  get 'users/account/edit', to: 'users#account_edit'
+  patch 'users/account/update', to: 'users#account_update'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
