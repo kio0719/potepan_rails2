4066e6aa07602f1a8fef00f67228d8c9a39a2d98
ioioio0719 <kio0719@docomo.ne.jp>
2023-01-22 13:53:10 +0900

    feat: Reservationのバリデーション機能を追加

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,13 +3,13 @@ class ReservationsController < ApplicationController
     @reservations = Reservation.where(user_id: current_user)
   end
   def confirm
-    @reservation = Reservation.new(params.permit(:check_in,:check_out,:users_num,:user_id,:room_id))
+    @reservation = Reservation.new(reservation_params)
     if @reservation.invalid?
-      render template: "rooms/edit"
+      @room = Room.find(@reservation.room_id)
+      render "rooms/show",{id: @reservation.room_id}
+    else
+      total_fee_date_calc
     end
-    @count_date = (params[:check_out].to_date - params[:check_in].to_date).to_i
-    room = Room.find(params[:room_id])
-    @total_fee = room.fee.to_i * params[:users_num].to_i * @count_date
   end
   def create
     @reservation = Reservation.new(reservation_params)
@@ -24,12 +24,12 @@ class ReservationsController < ApplicationController
   end
   def edit_confirm
     @reservation = Reservation.find(params[:id])
-    @reservation.attributes =  reservation_params
+    @reservation.attributes = reservation_params
     if @reservation.invalid?
       render "edit"
+    else
+      total_fee_date_calc
     end
-    @count_date = (@reservation.check_out - @reservation.check_in).to_i
-    @total_fee = @reservation.room.fee * @reservation.users_num * @count_date
   end
   def update
     @reservation = Reservation.find(params[:id])
@@ -47,5 +47,9 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.require(:reservation).permit(:check_in,:check_out,:users_num,:user_id,:room_id)
+  end
+  def total_fee_date_calc
+    @count_date = (@reservation.check_out - @reservation.check_in).to_i
+    @total_fee = @reservation.room.fee * @reservation.users_num * @count_date
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,15 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates :users_num, presence: true, numericality: true
+  validate :checkin_checkout_check
+
+  def checkin_checkout_check
+    if check_in && check_out
+      errors.add(:check_out,"はチェックイン以降の日付を記入してください。") if self.check_in >= self.check_out
+    end
+  end
 end
